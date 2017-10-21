1c7998de876a1e09f1990d3eb387b16f8ea672b7
matthew-morris <matthew.morris@aggiemail.usu.edu>
2017-10-20 20:55:14 -0600

    Added some animations and mouse recognition

--- javascript/Gameplay.js
+++ javascript/Gameplay.js
@@ -27,18 +27,6 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
   var backgroundImage = new Image();
   backgroundImage.src = "images/full-background.png";
 
-  window.onmousemove = function(e) {
-    //character.getMousePos(e.clientX, e.clientY);
-  };
-
-  const keys = [];
-  document.body.addEventListener("keyup", function(e) {
-    keys[e.keyCode] = false;
-  });
-  document.body.addEventListener("keydown", function(e) {
-    keys[e.keyCode] = true;
-  });
-
   function initialize() {
 
   }
@@ -99,10 +87,10 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
     Engine.run(engine);
     Render.run(render);
 
-    //open();
+    open();
     requestAnimationFrame(gameLoop); //starts game loop
 
-    document.getElementById('mainMenuButton').addEventListener('click', function() {Render.stop(render); World.clear(engine.world); Engine.clear(engine); Game.showScreen('menu');});
+    //document.getElementById('mainMenuButton').addEventListener('click', function() {Render.stop(render); World.clear(engine.world); Engine.clear(engine); MYGAME.Game.showScreen('menu');});
   }
 
   function createCharacter() {
@@ -187,6 +175,71 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
     this.numTouching = 0;
     this.crouch = false;
     this.isHeadClear = true;
+    this.flipBody = -1;
+    this.mouse = {
+      x: canvas.width / 3,
+      y: canvas.height
+    }
+    this.getMousePos = function(x,y) {
+      this.mouse.x = x;
+      this.mouse.y = y;
+    }
+    this.testingMoveLook = function() {
+      //move
+      this.x = player.position.x;
+      this.y = playerBody.position.y - this.yOff;
+      this.Vx = player.velocity.x;
+      this.Vy = player.velocity.y;
+      //look
+      this.canvasX = canvas.width / 2
+      this.canvasY = canvas.height / 2
+      this.transX = this.canvasX - this.x;
+      this.transY = this.canvasY - this.y;
+      this.angle = Math.atan2(this.mouse.y - this.canvasY, this.mouse.x - this.canvasX);
+    }
+    this.look = function() {
+      //set a max on mouse look
+      let mX = this.mouse.x;
+      if (mX > canvas.width * 0.8) {
+        mX = canvas.width * 0.8;
+      } else if (mX < canvas.width * 0.2) {
+        mX = canvas.width * 0.2;
+      }
+      let mY = this.mouse.y;
+      if (mY > canvas.height * 0.8) {
+        mY = canvas.height * 0.8;
+      } else if (mY < canvas.height * 0.2) {
+        mY = canvas.height * 0.2;
+      }
+      //set mouse look
+      this.canvasX = this.canvasX * 0.94 + (canvas.width - mX) * 0.06;
+      this.canvasY = this.canvasY * 0.94 + (canvas.height - mY) * 0.06;
+      //set translate values
+      this.transX = this.canvasX - this.x;
+      this.Sy = 0.99 * this.Sy + 0.01 * (this.y);
+      //hard caps how behind y position tracking can get.
+      if (this.Sy - this.y > canvas.height/2){
+         this.Sy = this.y + canvas.height/2
+      } else if (this.Sy - this.y < -canvas.height/2){
+        this.Sy = this.y - canvas.height/2
+      }
+      this.transY = this.canvasY - this.Sy;
+      //make player head angled towards mouse
+      this.angle = Math.atan2(this.mouse.y - this.canvasY, this.mouse.x - this.canvasX);
+
+      if (this.mouse.x < this.x ) {
+        if (this.flipBody == -1) {
+          Body.rotate(player, Math.PI);
+          this.flipBody =1;
+        }
+      }
+      else if (this.mouse.x > this.x) {
+        if (this.flipBody == 1) {
+          Body.rotate(player, Math.PI);
+          this.flipBody = -1;
+        }
+      }
+    };
     this.spawnPos = {
       x: Math.floor(Math.random() * 1000),
       y: 200
@@ -223,6 +276,11 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
     }
     this.keyMove = function() {
       if (this.onGround) { //on ground **********************
+        if (player.velocity.x <= 3) {
+          if (characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_idle.png") {
+            characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_idle.png";
+          }
+        }
         if ((keys[32] || keys[38] || keys[87]) && this.buttonCD_jump + 20 < game.cycle) { //jump
           this.buttonCD_jump = game.cycle; //can't jump until 20 cycles pass
           Matter.Body.setVelocity(player, { //zero player velocity for consistant jumps
@@ -234,19 +292,45 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
         //horizontal move on ground
         if (keys[37] || keys[65]) { //left or a
           if (player.velocity.x > -this.VxMax) {
+            this.walk_cycle += 0.05;
+            if (this.walk_cycle == 0.2) {
+              if ( characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_walk1.png") {
+                characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_walk1.png";
+              }
+            }
+            else if (this.walk_cycle >= 0.4) {
+              if ( characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_walk2.png") {
+                characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_walk2.png";
+                this.walk_cycle = 0;
+              }
+            }
             player.force.x = -this.Fx / game.delta;
           }
         } else if (keys[39] || keys[68]) { //right or d
+          this.walk_cycle += 0.05;
+          if (this.walk_cycle == 0.2) {
+            if ( characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_walk1.png") {
+              characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_walk1.png";
+            }
+          }
+          else if (this.walk_cycle >= 0.4) {
+            if ( characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_walk2.png") {
+              characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_walk2.png";
+              this.walk_cycle = 0;
+            }
+          }
           if (player.velocity.x < this.VxMax) {
             player.force.x = this.Fx / game.delta;
           }
         }
 
       } else { // in air **********************************
+        if ( characterBody.render.sprite.texture != "images/PNG/Player/Poses/player_jump.png") {
+          characterBody.render.sprite.texture = "images/PNG/Player/Poses/player_jump.png";
+        }
         //check for short jumps
         if (this.buttonCD_jump + 60 > game.cycle && //just pressed jump
-          !(keys[32] || keys[38] || keys[87]) && //but not pressing jump key
-          this.Vy < 0) { // and velocity is up
+          !(keys[32] || keys[38] || keys[87]) && this.Vy < 0) { // and velocity is up, but not pressing jump key
           Matter.Body.setVelocity(player, { //reduce player velocity every cycle until not true
             x: player.velocity.x,
             y: player.velocity.y * 0.94
@@ -292,7 +376,7 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
     };
 
     this.draw = function() {
-    }
+    };
   }
 
   function playerOnGroundCheck(event) { //runs on collisions events
@@ -344,11 +428,38 @@ MYGAME.screens['gameplay'] = (function (Game, Input) {
   }
 
   function gameLoop() {
+    /*
+    game.timing();
+    character.keyMove();
+    character.draw();
+    */
+
     game.timing();
     character.keyMove();
 
+    character.move();
+    character.look();
+    context.save();
+    context.translate(character.transX, character.transY);
+    character.draw();
+    context.restore();
+
     requestAnimationFrame(gameLoop);
   }
+
+  window.onmousemove = function(e) {
+    if (character != null ) {
+      character.getMousePos(e.clientX, e.clientY);
+    }
+  };
+
+  const keys = [];
+  document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+  });
+  document.body.addEventListener("keydown", function(e) {
+    keys[e.keyCode] = true;
+  });
 
   return {
     initialize: initialize,
